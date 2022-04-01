03cc4cb48d8a5f3ebc00f5a6a53d2f742bd446cf
blizzwind <50780897+blizzwind@users.noreply.github.com>
2022-04-01 09:55:44 +0800

    edited

--- db.py
+++ db.py
@@ -2,26 +2,29 @@ n = 1
 
 def exc(d, q):
     global n
+    t = []
     if q.startswith("add"):
-        q = q.split()
-        if len(q) != 2 and q[0] != "add":
+        q = q.split(" ")
+        if len(q) != 2 or q[0] != "add":
             return "error"
-        try:
-            d[str(n)] = q[1]
-        except:
-            return "error"
-        n = n + 1
-        return "added"
+        else:
+            try:
+                d[str(n)] = q[1]
+            except:
+                return "error"
+            n = n + 1
+            return "added"
     elif q.startswith("query"):
-        q = q.split()
-        if len(q) != 2 and q[0] != "query":
+        q = q.split(" ")
+        if len(q) != 2 or q[0] != "query":
             return "error"
-        k = list(d.keys())
-        v = list(d.values())
-        try:
-            p = v.index(q[1])
-        except:
-            return "error"
-        return k[p]
+        else:
+            try:
+                for key, value in d.items():
+                    if q[1] == value:
+                        t.append(key)
+            except:
+                return "error"
+            return t
     else:
         return "error"
